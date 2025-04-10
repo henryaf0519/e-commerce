f56736eb351c8849259c42c67821a9409b878bcf
henrya0519 <henrya0519@hotmail.com>
2025-04-10 14:29:35 -0500

    create button in logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
   { path: 'products', loadChildren: () => import('./modules/product/product.module').then(m => m.ProductModule)},
   { path: 'cart', loadChildren: () => import('./modules/shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule)},
   { path: '', redirectTo: '/products', pathMatch: 'full' },
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
   isUserMenuOpen = false;
 
   // Método para alternar la visibilidad del menú
   toggleUserMenu() {
     this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+  goToHome(){
+    this.router.navigate(['/products']);
   }
 }
