e312e72899817e07881ad14f2afaf27c1e595ee2
henryaf0519 <97894196+henryaf0519@users.noreply.github.com>
2025-06-09 12:01:10 -0500

    Fix inventory-list component initialization

--- src/app/modules/admin/components/inventory-list/inventory-list.component.ts
+++ src/app/modules/admin/components/inventory-list/inventory-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { InventoryServiceService, InventoryItem } from 'src/app/services/inventory-service.service';
 
@@ -8,21 +8,20 @@ import { InventoryServiceService, InventoryItem } from 'src/app/services/invento
   templateUrl: './inventory-list.component.html',
   styleUrls: ['./inventory-list.component.scss']
 })
-export class InventoryListComponent {
+export class InventoryListComponent implements OnInit {
 
   items: InventoryItem[] = [];
   
   constructor(
-    private inventoryService: InventoryServiceService, 
-    private router: Router) 
-    {}
-  
-    create() { this.router.navigate(['admin/create']); }
-    edit(id: string) { this.router.navigate(['admin/edit', id]); }
-    remove(id: string) { this.inventoryService.removeItem(id); }
+    private inventoryService: InventoryServiceService,
+    private router: Router
+  ) {}
 
+  ngOnInit() {
+    this.items = this.inventoryService.getItems();
+  }
 
-    
-
-
+  create() { this.router.navigate(['admin/create']); }
+  edit(id: string) { this.router.navigate(['admin/edit', id]); }
+  remove(id: string) { this.inventoryService.removeItem(id); }
 }
