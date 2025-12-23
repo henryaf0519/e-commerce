d9d85029888409fbfdd65943c7db80e3267b2ccf
henryaf0519 <henryaf0519@gmail.com>
2025-12-23 15:41:22 -0500

    estilos globales

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        blueMain: '#005fbe',
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        accent: 'rgb(var(--color-accent) / <alpha-value>)',
+        blueMain: 'rgb(var(--color-primary) / <alpha-value>)',
       },
     },
   },
