27236d1208da9d1dfb14bd90583a3ff746e16549
henryaf0519 <henryaf0519@gmail.com>
2025-12-29 17:37:52 -0500

    refactor: update footer and header components for improved layout and styling; add WhatsApp component with tooltip functionality

---
+++ src/app/shared/components/whatsapp/whatsapp.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-whatsapp',
+  templateUrl: './whatsapp.component.html',
+  styleUrls: ['./whatsapp.component.scss']
+})
+export class WhatsappComponent {
+  @Input() phone: string = '573196372542'; 
+  @Input() message: string = 'Hola, estoy interesado en los dulces de Dulcilandia.';
+  
+  showTooltip: boolean = false;
+
+  get whatsappLink(): string {
+    const encodedMessage = encodeURIComponent(this.message);
+    return `https://wa.me/${this.phone}?text=${encodedMessage}`;
+  }
+
+}
