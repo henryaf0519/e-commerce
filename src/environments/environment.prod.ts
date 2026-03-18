d5d71b35d14fca18481bee0fc20118670714a268
henryaf0519 <henryaf0519@gmail.com>
2026-03-18 14:48:09 -0500

    data enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://pruebasfaciles.online/v2',
-  businessId: 'tienda-de-henry',
+  businessId: 'root&CaneCO',
   strippeKey:'pk_live_51SZhHTJ51nohVpnZ0iAa3JuhZiLaoc0gZaiIlrZHNebm9QemVGI3db7eaVSa93y44J04KPdqlTaVSCpzVF2HQdfJ00vhCixMsA',
   defaultLanguage: 'es'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000',
-  businessId: 'tienda-de-henry',
+  businessId: 'root&CaneCO',
   strippeKey:'pk_test_51SkApyFfzAYNFajOAIL0kAt7vyOv85QLOWJcj1B5JLFjbhDqVIvvF7BhaJEhsYFbdQdPAluROwnQcVeUNUC0eKWg00mrd5YjDD',
   defaultLanguage: 'es'
 }
